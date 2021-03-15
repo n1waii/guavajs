8c4944c268f4658cd6707604bbe9ae15a5981816
Alphexus <ndahal2024@cvsd.net>
2021-03-15 13:01:10 +0000

    quad tree implementatiom

--- guava/structures/quadtree.js
+++ guava/structures/quadtree.js
@@ -15,9 +15,7 @@ export default class Quad {
         // edge cases
         console.assert(node !== null, "Argument 1 is null expected node object");
 
-        if (!inBoundary(node.pos)) {
-            return;
-        }
+        if (!this.inBoundary(node.pos)) return;
 
         if (Math.abs(this.topLeft[0] - this.bottomRight[0]) <= 1 
         && Math.abs(this.topLeft[1] - this.bottomRight[1]) <= 1) { 
@@ -38,16 +36,81 @@ export default class Quad {
                         [
                             (this.topLeft[0] + this.bottomRight[0]) / 2, 
                             (this.topLeft[1] + this.bottomRight[1]) / 2
-                        ],
-                        node
+                        ]
                     ); 
-                } else {
                    this.topLeftTree.insert(node); 
                 }
+            // bottom left  
             } else {
-                // bottom left
+                if (this.bottomLeftTree === null) {
+                    this.bottomLeftTree = new Quad( 
+                        [this.topLeft[0], (this.topLeft[1]+this.bottomRight[1])/2), 
+                        [
+                            (this.topLeft[0] + this.bottomRight[0]) / 2, 
+                            this.bottomRight[1]
+                        ]
+                    ); 
+                   this.bottomLeftTree.insert(node); 
+                }
+            }
+        // right half
+        } else {
+            // top right
+            if ((this.topLeft[1] + this.bottomRight[1]) / 2 >= node.pos[1]) { 
+                if (this.topRightTree === null) {
+                    this.topRightTree = new Quad( 
+                        [(this.topLeft[0]+this.bottomRight[0])/2, this.topLeft[1]), 
+                        [
+                            this.bottomRight[0], 
+                            (this.topLeft[1] + this.bottomRight[1]) / 2
+                        ]
+                    ); 
+                   this.topRightTree.insert(node); 
+                }
+            // bottom right  
+            } else {
+                if (this.bottomRightTree === null) {
+                    this.bottomRightTree = new Quad( 
+                        [this.topLeft[0], (this.topLeft[1]+this.bottomRight[1])/2), 
+                        [
+                            (this.topLeft[0] + this.bottomRight[0]) / 2, 
+                            this.bottomRight[1]
+                        ]
+                    ); 
+                   this.bottomRightTree.insert(node); 
+                }
             }
         }
+    }
+
+    search(p) {
+        if (!this.inBoundary(p)) return null;
+        if (this.node !== null) return this.node;
+
+        if ((this.topLeft[0] + this.bottomRight[0]) / 2 >= p[0]) { 
+            // top left
+            if ((this.topLeft[1] + this.bottomRight[1]) / 2 >= p[1]) { 
+                if (this.topLeftTree === null) return null;
+                return this.topLeftTree.search(p); 
+            } 
+            // bottom left 
+            else { 
+                if (this.bottomLeftTree === null) return null; 
+                return this.bottomLeftTree.search(p); 
+            } 
+        } 
+        else { 
+            // top right 
+            if ((this.topLeft[1] + this.bottomRight[1]) / 2 >= p[1]) { 
+                if (this.topRightTree === null) return null; 
+                return this.topRightTree.search(p); 
+            } 
+            // bottom right 
+            else { 
+                if (this.bottomRightTree === null) return null; 
+                return this.bottomRightTree.search(p); 
+            } 
+        } 
     }
 
     inBoundary(p) {
